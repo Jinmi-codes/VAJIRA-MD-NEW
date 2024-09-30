b4f3f7299464bc8e145a7ef2354037cb73b5cb23
Jinmi-codes <96597693+Jinmi-codes@users.noreply.github.com>
2024-09-30 16:48:22 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=jU8TDQBB#_4pvE_VtPcaqR6ymKL97zhIldohVAZdbxuswgfwZW0w' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=WNUU1RQI#DBPxMKB1ovVh4lDGp-V8z0pVNVyZKjx-Qw0OnbXClGg' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '2349022212703' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
