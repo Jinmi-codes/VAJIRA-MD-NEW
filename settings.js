52735fdb65052c6231370aec68ccf249471f599c
Jinmi-codes <96597693+Jinmi-codes@users.noreply.github.com>
2024-09-30 15:00:03 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=jU8TDQBB#_4pvE_VtPcaqR6ymKL97zhIldohVAZdbxuswgfwZW0w' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+2349022212703' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
@@ -40,6 +40,6 @@ HEROKU_API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HERO
 HEROKU_APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME,         
 LOGO: process.env.LOGO || `https://telegra.ph/file/cce13b56a460afc9f4139.jpg` ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,    
-ANTI_DELETE : process.env.ANTI_DELETE || false ,
-DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
+ANTI_DELETE : process.env.ANTI_DELETE || true ,
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '+2349022212703' : process.env.DELETEMSGSENDTO        
 };
